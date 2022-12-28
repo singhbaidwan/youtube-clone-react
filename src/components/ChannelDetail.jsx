03193ca863d9938bb63873a3dea7e8ad00ae5588
Dalveer Singh <59986053+singhbaidwan@users.noreply.github.com>
2022-12-28 18:13:30 +0530

    Side Bar and Nav Bar completed

---
+++ src/components/ChannelDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChannelDetail() {
+  return (
+    <div>
+      <h1>ChannelDetails</h1>
+    </div>
+  )
+}
+
+export default ChannelDetail
---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SearchFeed() {
+  return (
+    <div>
+      <h1>this is search feed</h1>
+    </div>
+  )
+}
+
+export default SearchFeed
